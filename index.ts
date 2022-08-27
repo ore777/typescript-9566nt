b631b7e7a67c7c6059f9bfd3057e335e90412f55
ore777 <rlhotakuggany@gmail.com>
2022-08-28 00:27:48 +0200

    2 files modified

--- index.ts
+++ index.ts
@@ -1,9 +1,6 @@
-let mynumber1, mynumber2: number;
-let mynumber3: number;
-mynumber1 = 100;
-mynumber2 = 200;
-mynumber3 = mynumber1
-mynumber1 = mynumber2
-mynumber2 = mynumber3
-console.log(mynumber1);
-console.log(mynumber2);
+let mynumber1, mynumber2, mynumber3: number;
+mynumber1 = parseInt( prompt ("enter your text here"));
+mynumber2 = parseInt ( prompt ("enter second your text here"));
+mynumber3 = mynumber1 + mynumber2;
+console.log(mynumber3)
+
