7f50984948cb3ee0055b740d87f6e15ee70bf657
ore777 <rlhotakuggany@gmail.com>
2022-08-27 21:19:19 +0200

    1 file modified

--- index.ts
+++ index.ts
@@ -1,10 +1,9 @@
-
-let num1: number;
-let num2: number;
-let num3: number;
-let num4: number;
-num1 = 10;
-num2 = 20;
-num3 = 30;
-num4 = num1 + num2 + num3 ;
-console.log(num4);
+let mynumber1, mynumber2: number;
+let mynumber3: number;
+mynumber1 = 100;
+mynumber2 = 200;
+mynumber3 = mynumber1
+mynumber1 = mynumber2
+mynumber2 = mynumber3
+console.log(mynumber1);
+console.log(mynumber2);
